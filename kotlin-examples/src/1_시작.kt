e962dabb1d37579a1638e0cf78a3fc07c9d483bf
Chansik Yun <chansigi@gmail.com>
2018-05-28 10:22:18 +0900

    .gitignore 파일 추가 및 프로젝트 생성

---
+++ kotlin-examples/src/1_시작.kt
@@ -0,0 +1,36 @@
+// 1_시작.kt
+package ex1
+
+// 1) 코틀린 언어 특징
+//   : 모던 프로그래밍 언어
+//     => Swift(Obj-C), Go(C), Kotlin(Java), Rust(C++)
+
+//   1. 간결함
+//     : 보일러플레이트 제거
+//   2. 안전함
+//   3. 상호 운용
+
+/*
+public class Program {
+    public static void main(String[] args) {
+        System.out.println("Hello, Java");
+    }
+}
+*/
+
+fun main(args: Array<String>) {
+    println("Hello, Kotlin")
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
