3640a077890264701dc932bc997c2c2d91a714d9
Chansik Yun <chansigi@gmail.com>
2018-05-30 13:26:49 +0900

    .

--- kotlin-examples/src/18_고차함수2.kt
+++ kotlin-examples/src/18_고차함수2.kt
@@ -1,14 +1,27 @@
 package ex18_2
 
 
+/*
 interface Predicate {
     fun test(e: Int): Boolean
 }
+*/
 
 // fun test(e: Int) : Boolean
 //  => (Int) -> Boolean
 
-fun filter(data: List<Int>, pred: (Int) -> Boolean): List<Int> {
+class User {
+    inner class A
+    inner class B
+}
+
+typealias UserA = User.A
+typealias UserB = User.B
+
+// Kotlin에서도 복잡한 타입의 표현을 간략화하는 문법을 제공합니다. - typealias
+typealias Predicate = (Int) -> Boolean
+
+fun filter(data: List<Int>, pred: Predicate): List<Int> {
     val result = mutableListOf<Int>()
 
     for (e in data) {
@@ -25,7 +38,10 @@ fun isEven(e: Int) = e % 2 == 0
 fun filterEvens(data: List<Int>) = filter(data, ::isEven)
 fun filterOdds(data: List<Int>) = filter(data, ::isOdd)
 
+fun greaterThan5(e: Int) = e >= 5
+
 fun main(args: Array<String>) {
+    // User.A
     val list = listOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
 
     val evens = filterEvens(list)
@@ -34,12 +50,44 @@ fun main(args: Array<String>) {
     println("evens: $evens")
     println("odds: $odds")
 
-    // 무명 객체를 생성해서 전달하는 방법
-    /*
-    val result = filter(list, object : Predicate {
-        override fun test(e: Int) = e >= 5
+    var result = filter(list, ::greaterThan5)
+
+
+    // 1. 익명(무명) 함수: 이름이 없는 함수
+    result = filter(list, fun(e: Int) = e >= 5)
+
+    // 2. 람다(Lambda): 코드 조각을 인자로 전달하는 기술
+    result = filter(list, { e: Int ->
+        e >= 5
     })
 
+    // 만약 호출하는 함수의 마지막 인자가 함수인 경우,
+    // 다음과 같이 표현하는 것도 가능합니다.
+    //  : Trailing Lambda
+    result = filter(list) { e: Int ->
+        e >= 5
+    }
+
+    // 람다의 인자는 타입이 추론이 가능합니다.
+    result = filter(list) { e ->
+        e >= 5
+    }
+
+    // 만약 람다의 인자가 1개인 경우, 인자 표현을 생략할 수 있습니다.
+    result = filter(list) { it >= 5 }
+
+
+
+
+
+
     println("result: $result")
-    */
 }
+
+
+
+
+
+
+
+
