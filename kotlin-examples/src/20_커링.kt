44291b9072e007ed788760bfc99b1a7e6f865a39
Chansik Yun <chansigi@gmail.com>
2018-05-30 15:53:21 +0900

    .

--- kotlin-examples/src/20_커링.kt
+++ kotlin-examples/src/20_커링.kt
@@ -10,21 +10,46 @@ package ex20
 // -> fun sum(a: Int)(b: Int)(c: Int) ; Scala
 
 // 일반 함수
-fun sum(a: Int, b: Int): Int = a + b
+
 
 // 1) 함수의 실행을 지연할 수 있다.
 // 2) 2개 커링 함수
-fun sum(a: Int): (Int) -> Int = { b ->
-    a + b
-}
+//fun sum(a: Int): (Int) -> Int = { b ->
+//    a + b
+//}
 
 // 3) 3개 커링 함수
+// sum(10)       : (Int) -> (Int) -> Int
+// sum(10)(20)
+// sum(30)(20)(30)
 
+//fun sum(a: Int): (Int) -> (Int) -> Int = { b ->
+//    { c ->
+//        a + b + c
+//    }
+//}
+
+// 인자가 2개인 함수에 대해서 커링 함수를 생성하는 고차 함수
+
+// (P1, P2) -> R
+
+fun sum(a: Int, b: Int): Int = a + b
+
+fun <P1, P2, R> ((P1, P2) -> R).curried(): (P1) -> (P2) -> R = { p1 ->
+    { p2 ->
+        this(p1, p2)
+    }
+}
 
 fun main(args: Array<String>) {
+    // val s = sum(10)(20)(30)
+    // println("sum: $s")
 
-    val s = sum(10)(20)
-    println("sum: $s")
+    // sum(10, 20)
+    val curriedSum = ::sum.curried()
+
+    val result = curriedSum(10)(20)
+    println("result: $result")
 }
 
 
