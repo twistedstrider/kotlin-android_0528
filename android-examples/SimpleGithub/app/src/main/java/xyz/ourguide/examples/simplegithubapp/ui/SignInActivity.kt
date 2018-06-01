a4e3ea1f001bc59ea54f396b1ecf858da9392f57
Chansik Yun <chansigi@gmail.com>
2018-06-01 13:58:43 +0900

    .

--- android-examples/SimpleGithub/app/src/main/java/xyz/ourguide/examples/simplegithubapp/ui/SignInActivity.kt
+++ android-examples/SimpleGithub/app/src/main/java/xyz/ourguide/examples/simplegithubapp/ui/SignInActivity.kt
@@ -4,6 +4,15 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import xyz.ourguide.examples.simplegithubapp.R
 
+// Social Login - Github Login
+//   1. Firebase Login - Github
+//       + FirebaseUI
+//    => OAuth2
+
+//   2. Github OAuth2 Login
+
+
+
 class SignInActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
