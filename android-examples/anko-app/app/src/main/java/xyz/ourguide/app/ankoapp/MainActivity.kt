f13f3aad7da665bbd432a5f9be01e80dd7e369c3
Chansik Yun <chansigi@gmail.com>
2018-06-01 13:40:06 +0900

    .

--- android-examples/anko-app/app/src/main/java/xyz/ourguide/app/ankoapp/MainActivity.kt
+++ android-examples/anko-app/app/src/main/java/xyz/ourguide/app/ankoapp/MainActivity.kt
@@ -5,6 +5,7 @@ import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v7.app.AlertDialog
+import android.util.Log
 import android.view.View
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
@@ -152,10 +153,22 @@ class MainActivity : AppCompatActivity() {
 // 2. XML로 정의된 뷰를 사용하는 경우, 파싱에 대한 비용이 소모됩니다.
 //    "성능", "배터리" => 코드를 통해 뷰를 구성할 수 있도록 하겠다.
 
-class MainActivity : AppCompatActivity() {
+// AnkoLogger: Logger
+//   'TAG'
+
+
+class MainActivity : AppCompatActivity(), AnkoLogger {
+//    companion object {
+//        val TAG = MainActivity::class.java.simpleName
+//    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+//        Log.i(TAG, "onCreate")
+        info("onCreate")
+        debug("onCreate")
+        error("onCreate")
+        wtf("onCreate")
 
 
         MainActivityUI().setContentView(this)
