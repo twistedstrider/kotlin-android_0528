a6cc2f44da92285edaf5ffb55c3b8892c209c589
Chansik Yun <chansigi@gmail.com>
2018-06-01 13:42:51 +0900

    .

---
+++ android-examples/SimpleGithub/app/src/main/java/xyz/ourguide/examples/simplegithubapp/ui/SignInActivity.kt
@@ -0,0 +1,13 @@
+package xyz.ourguide.examples.simplegithubapp.ui
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import xyz.ourguide.examples.simplegithubapp.R
+
+class SignInActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_in)
+    }
+}
