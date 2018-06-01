d28a0dc9021e52265be1d3b9aec1dcea4c20ecd8
Chansik Yun <chansigi@gmail.com>
2018-06-01 14:22:55 +0900

    .

--- android-examples/SimpleGithub/app/src/main/java/xyz/ourguide/examples/simplegithubapp/ui/SignInActivity.kt
+++ android-examples/SimpleGithub/app/src/main/java/xyz/ourguide/examples/simplegithubapp/ui/SignInActivity.kt
@@ -1,7 +1,11 @@
 package xyz.ourguide.examples.simplegithubapp.ui
 
+import android.net.Uri
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_sign_in.*
+import org.jetbrains.anko.toast
+import xyz.ourguide.examples.simplegithubapp.BuildConfig
 import xyz.ourguide.examples.simplegithubapp.R
 
 // Social Login - Github Login
@@ -15,12 +19,29 @@ import xyz.ourguide.examples.simplegithubapp.R
 //         GITHUB_CLIENT_SECRET
 //      app - gradle.properties
 
-
-
 class SignInActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_in)
+
+
+        btnActivitySignInStart.setOnClickListener {
+
+            // Github OAuth Login
+            // https://github.com/login/oauth/authorize?client_id=<ID>
+
+            val authUri = Uri.Builder().apply {
+                scheme("https")
+                authority("github.com")
+                appendPath("login")
+                appendPath("oauth")
+                appendQueryParameter("client_id", BuildConfig.GITHUB_CLIENT_ID)
+            }.build()
+
+            toast(authUri.toString())
+
+        }
+
     }
 }
