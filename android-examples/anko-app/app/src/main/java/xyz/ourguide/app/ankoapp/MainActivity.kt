e8e09379178fcb55ec0b83eaa5bf40a9a0423262
Chansik Yun <chansigi@gmail.com>
2018-06-01 11:33:37 +0900

    .

--- android-examples/anko-app/app/src/main/java/xyz/ourguide/app/ankoapp/MainActivity.kt
+++ android-examples/anko-app/app/src/main/java/xyz/ourguide/app/ankoapp/MainActivity.kt
@@ -1,9 +1,13 @@
 package xyz.ourguide.app.ankoapp
 
+import android.content.DialogInterface
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AlertDialog
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
+import org.jetbrains.anko.longToast
+import org.jetbrains.anko.toast
 
 // Anko
 //  : Jetbrains 직접 제작하여 배포하는 코틀린 라이브러리
@@ -28,8 +32,24 @@ class MainActivity : AppCompatActivity() {
 
 
         button.setOnClickListener {
+            // 1. Toast
             // Toast.makeText(this, "Toast!", Toast.LENGTH_SHORT).show()
             // toast("Toast!")
+            // longToast("Toast!")
+
+            // 2. AlertDialog
+            val dialog = AlertDialog.Builder(this)
+                    .setTitle("Dialog Title")
+                    .setMessage("Dialog Message")
+                    .setPositiveButton("Yes") { _, _ ->
+                        toast("Yes")
+                    }
+                    .setNegativeButton("No") { _, _ ->
+                        toast("No")
+                    }
+                    .create()
+            dialog.show()
+
         }
 
     }
