7d4b65f732f2c8d38fffe16dd323c3baf48cff4b
In-Jik Lee <injik.lee@In-Jikui-MacBookAir.local>
2024-04-13 14:42:53 +0900

    when expression without block

--- kotlin.in.action/list_002_018.kt
+++ kotlin.in.action/list_002_018.kt
@@ -2,6 +2,13 @@ interface Expr;
 class Num(val value: Int) : Expr;
 class Sum(val left: Expr, val right: Expr) : Expr;
 
+fun eval(e: Expr) : Int =
+    when(e) {
+        is Num -> e.value
+        is Sum -> eval(e.left) + eval(e.right)
+        else -> throw IllegalArgumentException("Unknown expression")
+    }
+
 fun evalWithLogging(e: Expr) : Int =
 when(e) {
     is Num -> {
@@ -19,5 +26,8 @@ when(e) {
 
 fun main_list_002_018(args: Array<String>)
 {
+    println(args);
+
+    println(eval(Sum(Sum(Num(1), Num(2)), Num(4))));
     println(evalWithLogging(Sum(Sum(Num(1), Num(2)), Num(4))));
 }
